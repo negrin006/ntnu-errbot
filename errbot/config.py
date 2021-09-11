633cefd50a0222767261bd067a373737050ad5c1
Muriel Negrin <negrin006@outlook.com>
2021-09-11 18:37:34 +0800

    students

--- errbot/config.py
+++ errbot/config.py
@@ -5,7 +5,11 @@ import logging
 # the options in the more complete config-template.py from here:
 # https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py
 
-BACKEND = 'Text'  # Errbot will start in text mode (console only mode) and will answer commands from there.
+BACKEND = 'Slack'  # Errbot will start in text mode (console only mode) and will answer commands from there.
+
+BOT_IDENTITY = {
+    'token' : 'xoxb-2465499322295-2486435335204-lGQhb5LSi4P2BWbP1SUGgELZ'
+}
 
 BOT_DATA_DIR = r'D:\Scratch\NTNU-ErrBot\errbot\data'
 BOT_EXTRA_PLUGIN_DIR = r'D:\Scratch\NTNU-ErrBot\errbot\plugins'
@@ -13,4 +17,4 @@ BOT_EXTRA_PLUGIN_DIR = r'D:\Scratch\NTNU-ErrBot\errbot\plugins'
 BOT_LOG_FILE = r'D:\Scratch\NTNU-ErrBot\errbot\errbot.log'
 BOT_LOG_LEVEL = logging.DEBUG
 
-BOT_ADMINS = ('@CHANGE_ME', )  # !! Don't leave that to "@CHANGE_ME" if you connect your errbot to a chat system !!
+BOT_ADMINS = ('@ntnuerc', )  # !! Don't leave that to "@CHANGE_ME" if you connect your errbot to a chat system !!
